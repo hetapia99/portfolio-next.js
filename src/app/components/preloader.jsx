a7ce3e49d6a10bc051a875828608b8fd2091a4dc
hetapia99 <meme.tapia@icloud.com>
2024-05-13 13:45:49 -0500

    animation name and bg color change

--- src/app/components/preloader.jsx
+++ src/app/components/preloader.jsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link'
 import React, {useEffect} from 'react'
-import { preLoaderAnim } from './animations/index.jsx';
+import { preLoaderAnim } from '../../animations/index.js';
 import './preloader.css'
 
 const Preloader = () => {
